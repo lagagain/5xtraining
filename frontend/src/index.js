32fedbafcee010a4ac40e174d1e2d548848e1036
lagagain <lagagain@email.com>
2020-03-06 17:36:45 +0800

    update: 改用React改寫

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ var $ = require('jquery');
 window.$ = $;
 
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
