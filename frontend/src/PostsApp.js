e97256cac34ba4ea93e2ae54fc74824d4ef1f85d
lagagain <lagagain@email.com>
2020-03-12 18:37:51 +0800

    update: fetch all posts and users, then show 1st ~ 4th posts.

--- frontend/src/PostsApp.js
+++ frontend/src/PostsApp.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 
 
@@ -54,128 +55,20 @@ class List extends React.Component {
                   </div>
                 </div>
            </div>);
-    posts = [
-        (<div className="posts-lists-partial mt-5">
-          <div className="container">
-            <div className="row">
-              <div className="col-12 center-block">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-                  <div className="posts-wrap d-flex light-grey-bg">
-                    <div className="cover col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-0 pr-0 img-h clickable"
-                         data-href="/posts/react-native-upgrade-60"
-                         style={{
-                             "background": `url("https://picsum.photos/540/305?random=${Math.ceil(Math.random()*1000)}")`,
-                             "background-size": "cover",
-                             "background-position": "center center",
-                         }}
-                    >
-                    </div>
-                    <div className="content col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-4 pr-4 pt-4 pb-4">
-                      <div className="text-grey small">Mar 10 2020</div>
-                      <div className="mt-2">
-                        <h3>
-                          <a href="/posts/react-native-upgrade-60">
-                            React Native 升級教戰手冊 — 以 0.60 + AppBundle 為例
-                          </a>
-                        </h3>
-                      </div>
-                      <div className="summary mt-3 mb-3">
-                        <p>
-                          在開發 React Native （以下簡稱 RN） App 期間除了遇到很多雷之外，大致功能做完開始進入長期維護狀態也是偶爾就會有平台的問題冒出來得處理，有時候是 Apple/Google Play 改了些什麼會要求開發者某時候開始的 App 必須符合某些新規範或是 native API 改動，像是前陣子 RN App 用新版的 XCode build 出來的 App 跑起來會直接閃退，一查發現是 RN 自己使用的 iOS API 過舊導致的，但是修正好的 patch 只有在更新的 RN...
-                        </p>
-                      </div>
-                      <div className="footer">
-                        <span className="author text-grey small">By PastLeo</span>
-                        <a href="/posts/react-native-upgrade-60" className="more">看更多</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-         </div>),
-        (<div className="posts-lists-partial mt-5">
-          <div className="container">
-            <div className="row">
-              <div className="col-12 center-block">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-                  <div className="posts-wrap d-flex light-grey-bg">
-                    <div className="cover col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-0 pr-0 img-h clickable"
-                         data-href="/posts/react-native-upgrade-60"
-                         style={{
-                             "background": `url("https://picsum.photos/540/305?random=${Math.ceil(Math.random()*1000)}")`,
-                             "background-size": "cover",
-                             "background-position": "center center",
-                         }}
-                    >
-                    </div>
-                    <div className="content col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-4 pr-4 pt-4 pb-4">
-                      <div className="text-grey small">Mar 10 2020</div>
-                      <div className="mt-2">
-                        <h3>
-                          <a href="/posts/react-native-upgrade-60">
-                            React Native 升級教戰手冊 — 以 0.60 + AppBundle 為例
-                          </a>
-                        </h3>
-                      </div>
-                      <div className="summary mt-3 mb-3">
-                        <p>
-                          在開發 React Native （以下簡稱 RN） App 期間除了遇到很多雷之外，大致功能做完開始進入長期維護狀態也是偶爾就會有平台的問題冒出來得處理，有時候是 Apple/Google Play 改了些什麼會要求開發者某時候開始的 App 必須符合某些新規範或是 native API 改動，像是前陣子 RN App 用新版的 XCode build 出來的 App 跑起來會直接閃退，一查發現是 RN 自己使用的 iOS API 過舊導致的，但是修正好的 patch 只有在更新的 RN...
-                        </p>
-                      </div>
-                      <div className="footer">
-                        <span className="author text-grey small">By PastLeo</span>
-                        <a href="/posts/react-native-upgrade-60" className="more">看更多</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-         </div>),
-        (<div className="posts-lists-partial mt-5">
-          <div className="container">
-            <div className="row">
-              <div className="col-12 center-block">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-                  <div className="posts-wrap d-flex light-grey-bg">
-                    <div className="cover col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-0 pr-0 img-h clickable"
-                         data-href="/posts/react-native-upgrade-60"
-                         style={{
-                             "background": `url("https://picsum.photos/540/305?random=${Math.ceil(Math.random()*1000)}")`,
-                             "background-size": "cover",
-                             "background-position": "center center",
-                         }}
-                    >
-                    </div>
-                    <div className="content col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-4 pr-4 pt-4 pb-4">
-                      <div className="text-grey small">Mar 10 2020</div>
-                      <div className="mt-2">
-                        <h3>
-                          <a href="/posts/react-native-upgrade-60">
-                            React Native 升級教戰手冊 — 以 0.60 + AppBundle 為例
-                          </a>
-                        </h3>
-                      </div>
-                      <div className="summary mt-3 mb-3">
-                        <p>
-                          在開發 React Native （以下簡稱 RN） App 期間除了遇到很多雷之外，大致功能做完開始進入長期維護狀態也是偶爾就會有平台的問題冒出來得處理，有時候是 Apple/Google Play 改了些什麼會要求開發者某時候開始的 App 必須符合某些新規範或是 native API 改動，像是前陣子 RN App 用新版的 XCode build 出來的 App 跑起來會直接閃退，一查發現是 RN 自己使用的 iOS API 過舊導致的，但是修正好的 patch 只有在更新的 RN...
-                        </p>
-                      </div>
-                      <div className="footer">
-                        <span className="author text-grey small">By PastLeo</span>
-                        <a href="/posts/react-native-upgrade-60" className="more">看更多</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-         </div>),
-    ]
+
+    constructor(props){
+        super(props);
+        this.getAllPosts()
+            .then(this.updateShowPosts);
+        this.getAllUser();
+        window.tmp = this;
+
+        this.state = {
+            posts: [],
+            users: {},
+        };
+    }
+
     toggleCategoryBtn = (e) => {
         let target = e.target;//e.currentTarget;
         let parent = target.parentNode;
@@ -186,14 +79,91 @@ class List extends React.Component {
         return false;
     };
 
+    getAllPosts = async () => {
+        let posts = await fetch('https://jsonplaceholder.typicode.com/posts');
+        posts = await posts.json();
+        this.posts = posts;
+        return posts;
+    };
+
+    getAllUser = async () => {
+        let users = await fetch('https://jsonplaceholder.typicode.com/users/');
+        users = await users.json();
+        this.users = users;
+        this.setState({users});
+    }
+
+    updateShowPosts = async () => {
+        if(this.posts){
+            let posts = [];
+            for(let i = 0; i < 4; i++){
+                let post = this.posts[i];
+                let author_id = post.userId;
+                let author = new Proxy(this.state.users[author_id - 1] || {},
+                                       {
+                                           get(target, key){
+                                               return (key in target) ? target[key] : "Not Found";
+                                           },
+                                       });
+
+                posts.push(
+                    (<div className="posts-lists-partial mt-5">
+                       <div className="container">
+                         <div className="row">
+                           <div className="col-12 center-block">
+                             <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
+                               <div className="posts-wrap d-flex light-grey-bg">
+                                 <div className="cover col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-0 pr-0 img-h clickable"
+                                      data-href="/posts/react-native-upgrade-60"
+                                      style={{
+                                          "background": `url("https://picsum.photos/540/305?random=${Math.ceil(Math.random()*1000)}")`,
+                                          "background-size": "cover",
+                                          "background-position": "center center",
+                                      }}
+                                 >
+                                 </div>
+                                 <div className="content col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-4 pr-4 pt-4 pb-4">
+                                   <div className="text-grey small">Mar 10 2020</div>
+                                   <div className="mt-2">
+                                     <h3>
+                                       <a href="#post_id">
+                                         { post.title }
+                                       </a>
+                                     </h3>
+                                   </div>
+                                   <div className="summary mt-3 mb-3">
+                                     <p>
+                                       { post.body }
+                                     </p>
+                                   </div>
+                                   <div className="footer">
+                                     <span className="author text-grey small">By { author.username }</span>
+                                     <a href="post_id" className="more">看更多</a>
+                                   </div>
+                                 </div>
+                               </div>
+                             </div>
+                           </div>
+                         </div>
+                       </div>
+                     </div>)
+                );
+            }
+            this.setState({posts});
+        }else{
+            this.getAllPosts()
+                .then(this.updateShowPosts);
+        }
+    }
+
     render(){
         return (
             <div>
               <Banner></Banner>
 
               {this.nav}
-              
-              {this.posts}
+
+              {this.state.posts}
 
               <div>Pagination</div>
             </div>
